504044820537d251cd48873ee882f0ba37b8d78d
muskan-015 <muskanvds2004@gmail.com>
2025-01-07 20:33:22 +0530

    adding new file

---
+++ div.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class helloworld{
+    public static void main(String []args){
+Scanner sc=new Scaneer(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+
+        System.out.println("Division is:"+(a/b));
+            }
+}
